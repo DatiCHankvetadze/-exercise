debdbdec1e237e68f349f455f740b2d6c7250925
DatiChankvetadze <Datichankvetadze@gmail.com>
2024-01-01 19:07:18 +0400

    My Portfolio

---
+++ py/def.py
@@ -0,0 +1,16 @@
+#სასტუმროში შემოდის ოთხი ბიჭი და გვინდა ოთხივეს დავუპრინტოთ მოგესალმებით დღეში სამჯერ
+
+def bichebtan_misalmeba():
+    print("hello Mate")
+    print("hello Gio")
+    print("hello Nika")
+    print("hello Dati")
+
+print("dila")
+bichebtan_misalmeba()
+print("shua dge")
+bichebtan_misalmeba()
+print("sagamo")
+bichebtan_misalmeba()
+print("game")
+bichebtan_misalmeba()
